7f08c9a0dd37543a7329d004979b3e52f679e71e
codegarmx <codegarmx@gmail.com>
2024-05-31 17:54:18 -0600

    added helmet, rate limit and compression into the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { ThrottlerModule } from '@nestjs/throttler'
 import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { join } from 'path'
+
+// Import config files
+import { throttlerConfig } from '@app/config'
 
 // Import module own files
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { join } from 'path'
+
+// Features imports
 import { UsersModule } from './users/users.module'
 
 @Module({
@@ -15,12 +21,24 @@ import { UsersModule } from './users/users.module'
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
+      load: [throttlerConfig],
     }),
     // GraphQL
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       playground: true,
+    }),
+    // Throttler module (rate limit)
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => [
+        {
+          ttl: config.getOrThrow<number>('throttlerConfig.ttl'),
+          limit: config.getOrThrow<number>('throttlerConfig.limit'),
+        },
+      ],
     }),
     UsersModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core'
 import { ConfigService } from '@nestjs/config'
+import helmet from 'helmet'
+import * as compression from 'compression'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const config: ConfigService = app.get(ConfigService)
 
-  await app.listen(config.getOrThrow('LISTENING_PORT'))
+  app.use(
+    helmet({
+      contentSecurityPolicy:
+        config.get<string>('NODE_ENV') === 'production' ? undefined : false,
+    }),
+  )
+  app.use(compression())
+  await app.listen(config.getOrThrow<number>('LISTENING_PORT'))
 }
 bootstrap()
